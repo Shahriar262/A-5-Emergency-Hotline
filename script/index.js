5d2d9b228922ad8c485535553c9e93a39671c329
Shahriar262 <srh.2622@gmail.com>
2025-08-28 20:00:28 +0600

    added copy button functionality

--- script/index.js
+++ script/index.js
@@ -14,3 +14,29 @@ for(let i = 0; i < hearts.length; i++){
         hearts[i].classList.add('fa-solid', 'text-red-500'); 
     })
 }
+
+
+//copy count functionality
+
+const copy = document.querySelectorAll('.copy-btn');
+const copyCount = document.getElementById('copy-count');
+const numbers = document.querySelectorAll('.helpline-num');
+
+let totalCopyCount = 0;
+
+for(let i = 0; i < copy.length; i++){
+    copy[i].addEventListener('click', function(){
+        const numberText = numbers[i].innerText.trim();
+        navigator.clipboard.writeText(numberText)
+       .then(function(){
+         totalCopyCount++;
+         copyCount.innerText = totalCopyCount;
+         alert(`Hotline number ${numberText} copied!`);
+      })
+        
+      .catch(function(err){
+        alert(`Failed to copy ${err}`);
+      });
+        
+    })
+}
